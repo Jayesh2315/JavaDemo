2b09558e4a7efeebadd9085bf62de20c14b6b1a9
Jayesh2315 <jayuvr9@gmail.com>
2023-07-09 19:56:39 +0530

    Added a comment in file RanNum.java

--- RanNum.java
+++ RanNum.java
@@ -4,8 +4,8 @@ public class RanNum
 {
 	public static void main(String[] args) 
 	{
-		Random r=new Random();
-		int num=r.nextInt(1000);
+		Random r=new Random(); 
+		int num=r.nextInt(1000); //Random number between 1 and 1000 is stored in the variable num.
 		System.out.println(num);
 	}
 }
